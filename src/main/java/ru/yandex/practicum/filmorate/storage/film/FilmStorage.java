80b9ed249dbaf4f6bacbe5c4b7afba2740e709cf
Goshik <bokgosha@yandex.ru>
2026-02-04 01:23:19 +0300

    Исправить под checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -8,11 +8,18 @@ import java.util.List;
 public interface FilmStorage {
 
     List<Film> getPopularFilms(int count);
+
     void deleteLike(long userId, long filmId);
+
     Film saveLike(long userId, long filmId);
+
     Film getFilm(long filmId);
+
     Collection<Film> getFilms();
+
     Film saveFilm(Film film);
+
     Film updateFilm(Film film);
+
     void deleteFilm(long filmId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,12 +7,20 @@ import java.util.Collection;
 public interface UserStorage {
 
     Collection<User> getFriends(long userId);
+
     Collection<User> getCommonFriends(long user1Id, long user2Id);
+
     void deleteFriend(long userId, long friendId);
+
     User saveFriend(long userId, long friendId);
+
     Collection<User> getUsers();
+
     User getUser(long userId);
+
     User saveUser(User user);
+
     User updateUser(User user);
+
     void deleteUser(long userId);
 }
